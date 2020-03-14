c71701851cc4b34018b8edb2e886b6e16adda31d
yukou-isshiki <yff24517@nifty.com>
2020-03-14 22:32:16 +0900

    長野県版を追加

--- pages/government.vue
+++ pages/government.vue
@@ -11,6 +11,7 @@
       link="https://www.pref.kanagawa.jp/osirase/1369/"
     />
     <TextCard title="山梨県" link="https://stopcovid19.yamanashi.dev/" />
+    <TextCard title="長野県" link="https://stop-covid19-nagano.netlify.com/" />
     <TextCard title="岐阜県" link="https://covid19-gifu.netlify.com/" />
     <TextCard title="三重県" link="https://covid19-mie.netlify.com/" />
     <TextCard title="愛媛県" link="https://ehime-covid19.com/" />
